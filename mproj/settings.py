fbb154b3d34ab22fff898633ed3d54bd3b0668ba
MichaelHabermas <michaelghabermas@gmail.com>
2021-09-27 22:57:23 -0400

    hey

--- mproj/settings.py
+++ mproj/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = config('SECRET_KEY') #
 DEBUG = config('DEBUG', cast=bool) #
 
 # also in .env file
-ALLOWED_HOSTS = [] 
+ALLOWED_HOSTS = config('ALLOWED_HOSTS') #
 
 
 # Application definition
