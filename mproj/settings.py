27f720cb5cf2adfd9ac654d50e02e7754f5200b5
MichaelHabermas <michaelghabermas@gmail.com>
2021-09-30 21:46:34 -0400

    stuuff

--- mproj/settings.py
+++ mproj/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-# import os 
+import os #
 from pathlib import Path
 from decouple import config #
 import dj_database_url #
